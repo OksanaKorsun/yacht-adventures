67781e5c674f5d2c617d60db651cafc9eece8f0e
OksanaKorsun <oksana2081989@gmail.com>
2025-01-26 20:08:54 +0200

    section styles

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -10,30 +10,30 @@
 //   },
 // });
 
-const swiper = new Swiper('.swiper', {
-      slidesPerView: 3, 
-      spaceBetween: 16, 
-      loop: true, 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        1280: {
-          slidesPerView: 3, 
-          spaceBetween: 16, 
-        },
-        768: {
-          slidesPerView: 2, 
-          spaceBetween: 16,
-        },
-        375: {
-          slidesPerView: 1, 
-          spaceBetween: 10,
-        },
-      },
-    });
+// const swiper = new Swiper('.swiper', {
+//       slidesPerView: 3, 
+//       spaceBetween: 16, 
+//       loop: true, 
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//       breakpoints: {
+//         1280: {
+//           slidesPerView: 3, 
+//           spaceBetween: 16, 
+//         },
+//         768: {
+//           slidesPerView: 2, 
+//           spaceBetween: 16,
+//         },
+//         375: {
+//           slidesPerView: 1, 
+//           spaceBetween: 10,
+//         },
+//       },
+//     });
