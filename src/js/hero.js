9920c8ba2ce1630fc51e10c5527f0efac864dded
OksanaKorsun <oksana2081989@gmail.com>
2025-02-13 20:12:53 +0200

    fix animation

---
+++ src/js/hero.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelectorAll('.hero-image-container'), {
+  max: 15,
+  speed: 900,
+  glare: false,
+  transition: true,
+  
+});
--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import './js/swiper';
 import './js/modal.js';
 import './js/anchor-button.js';
 import './js/button-to-up.js';
+import './js/hero.js';
