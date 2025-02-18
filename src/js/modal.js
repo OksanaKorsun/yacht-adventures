a2b79d9a9d57085840d65f3cb27673be29e4eddb
OksanaKorsun <oksana2081989@gmail.com>
2025-02-18 12:17:41 +0200

    repeat fix form/modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.querySelector("[data-modal]");
-  const openBtn = document.querySelector("[data-modal-open]");
-  const closeBtn = document.querySelector("[data-modal-close]");
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.querySelector('[data-modal]');
+  const closeBtn = document.querySelector('[data-modal-close]');
   const body = document.body;
+  const form = document.getElementById('rent-form');
+  const nameInput = form.querySelector("[name='username']");
+  const modalGreeting = document.querySelector('[data-modal-greeting]');
 
   function openModal() {
-    modal.classList.add("active");
-    body.classList.add("no-scroll"); 
+    modal.classList.add('active');
+    body.classList.add('no-scroll');
   }
 
   function closeModal() {
-    modal.classList.remove("active");
-    body.classList.remove("no-scroll"); 
+    modal.classList.remove('active');
+    body.classList.remove('no-scroll');
   }
 
-  openBtn.addEventListener("click", openModal);
+  closeBtn.addEventListener('click', closeModal);
 
-  closeBtn.addEventListener("click", closeModal);
-  modal.addEventListener("click", function (e) {
-    if (e.target === modal) closeModal(); 
+  modal.addEventListener('click', function (e) {
+    if (e.target === modal) closeModal();
   });
 
-  document.getElementById('rent-form').addEventListener('submit', function(event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+
+    const userName = nameInput.value.trim();
+
+    modalGreeting.textContent = `${userName}, thank you!`;
+    setTimeout(() => {
+      form.reset();
+    }, 300);
     openModal();
   });
 });
