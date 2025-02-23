aeaab42c3069058dd0377f558f225f18cc5e2816
OksanaKorsun <oksana2081989@gmail.com>
2025-02-23 15:01:03 +0200

    mobile-menu

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './js/menu.js';
+import './js/mobile-menu.js';
 import './js/swiper';
 import './js/modal.js';
-import './js/anchor-button.js';
+// import './js/anchor-button.js';
 import './js/button-to-up.js';
-import './js/hero.js';
+// import './js/hero.js';
 // import './js/inputmask.js';
