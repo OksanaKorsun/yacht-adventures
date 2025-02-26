b5a916554555933a893d2c38d8e87e6d1e4270dd
OksanaKorsun <oksana2081989@gmail.com>
2025-02-26 11:36:32 +0200

    yahts adaptive

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,38 +1,26 @@
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const swiper = new Swiper(".swiper", {
-//     slidesPerView: 3, 
-//     spaceBetween: 16,
-//     direction: "horizontal",
-//     loop: true,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     breakpoints: {
-//       375: {
-//         slidesPerView: 1,
-//         spaceBetween: 20,
-//       },
-//       768: {
-//         slidesPerView: 2,
-//         spaceBetween: 16,
-//       },
-//       1280: {
-//           slidesPerView: 3,
-//           spaceBetween: 16,
-//       },
-//     },
-//   });
-// });
-window.onload = function() {
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 16,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1, 
+    spaceBetween: 10, 
+    direction: "horizontal",
     loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 16,
+      },
+    },
   });
-};
+});
