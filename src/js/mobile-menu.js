ce31c9df99d7549447bdffe756975169a298df42
OksanaKorsun <oksana2081989@gmail.com>
2025-03-14 13:32:17 +0200

    ellipses

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,6 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const menuLinks = document.querySelectorAll('.mobile-menu .nav-link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -19,16 +18,7 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закриваємо меню при кліку на якірне посилання
-  menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  });
-
-  // Закриваємо меню на широких екранах при зміні розміру вікна
+  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
